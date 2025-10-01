3d35bec0700419015c1ded52fb1a7c12dd9f144e
AadyaDubey <29aadyadubey@gmail.com>
2025-10-02 02:21:22 +0530

    Update

--- day8_Q15.c
+++ day8_Q15.c
@@ -1,28 +1,5 @@
 //Q15: Write a program to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
 
-/*
-Sample Test Cases:
-Input 1:
-A
-Output 1:
-
-
-Input 2:
-a
-Output 2:
-
-
-Input 3:
-3
-Output 3:
-
-
-Input 4:
-#
-Output 4:
-
-*/
-
 #include<stdio.h>
 
 int main (){
