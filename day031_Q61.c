811db539b06fe66f5c18bbcd72a1431a2f3ec55d
AadyaDubey <29aadyadubey@gmail.com>
2025-10-19 17:52:30 +0530

    Update

--- day031_Q61.c
+++ day031_Q61.c
@@ -16,3 +16,26 @@ Output 2:
 -1
 
 */
+
+#include<stdio.h>
+
+int main (){
+    int size,num;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&num);
+
+    int position = 0;
+    for(int i=0;i<size;i++){
+        if(arr[i]==num){
+            position = i+1;
+            break;
+        }
+    }
+
+    (position!=0)?printf("Found at index %d\n",position):printf("-1\n");
+    return 0;
+}
