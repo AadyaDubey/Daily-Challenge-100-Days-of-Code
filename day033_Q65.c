a7e91165b12cdb27bc2580121d7edd0b381715cc
AadyaDubey <29aadyadubey@gmail.com>
2025-11-09 19:21:17 +0530

    Update

--- day033_Q65.c
+++ day033_Q65.c
@@ -18,3 +18,41 @@ Output 2:
 
 */
 
+#include<stdio.h>
+int binary (int array[],int size,int find);
+
+int main (){
+    int size,find;
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&find);
+
+    int index = binary(arr,size,find);
+
+    if(index==-1)
+        printf("-1\n");
+    else
+        printf("Found at index %d\n",index);
+
+    return 0;
+}
+
+int binary (int array[],int size,int find){
+    int lower=0,upper=size-1;
+
+    while(lower<=upper){
+        int mid = lower + (upper-lower)/2;
+        if(array[mid]!=find){
+            if(array[mid]<find)
+                lower=mid+1;
+            else
+                upper=mid-1;
+        }else
+            return mid;
+    }
+
+    return -1;
+}
