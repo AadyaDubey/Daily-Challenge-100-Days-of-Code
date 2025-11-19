9178a47ed4ac6866dda339b15b61192cbff72ac5
AadyaDubey <29aadyadubey@gmail.com>
2025-11-20 00:50:20 +0530

    Update

--- day048_Q96.c
+++ day048_Q96.c
@@ -9,8 +9,34 @@ I evol gnidoc
 
 */
 
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(){
-    
+void reverse(char *start, char *end) {
+    while (start < end) {
+        char temp = *start;
+        *start = *end;
+        *end = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char arr[100];
+    fgets(arr, sizeof(arr), stdin); 
+    int i = 0, start = 0;
+
+    while (1) {
+        if (arr[i] == ' ' || arr[i] == '\0' || arr[i] == '\n') {
+            reverse(&arr[start], &arr[i - 1]);
+            start = i + 1;
+        }
+        if (arr[i] == '\0')
+            break;
+        i++;
+    }
+
+    printf("%s", arr);
+    return 0;
 }
