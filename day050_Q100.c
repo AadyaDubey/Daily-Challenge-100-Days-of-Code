1778c1966ff72a2012fdf35b7fbeed0d66c2343d
AadyaDubey <29aadyadubey@gmail.com>
2025-11-21 01:00:19 +0530

    Update

--- day050_Q100.c
+++ day050_Q100.c
@@ -20,7 +20,6 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         for (int j = i; j < n; j++) {
-            // Print substring from i to j
             for (int k = i; k <= j; k++) {
                 printf("%c", s[k]);
             }
