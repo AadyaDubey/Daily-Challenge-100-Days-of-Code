f1b98d49070f132b335e2fecd506a3bc8c1f9094
AadyaDubey <29aadyadubey@gmail.com>
2025-12-03 14:47:31 +0530

    Day 91-100

---
+++ day095_Q145.c
@@ -0,0 +1,45 @@
+// Q145: Return a structure containing top student's details from a function.
+
+/*
+Sample Test Cases:
+Input 1:
+3 students: Riya 101 89, Karan 102 96, Meena 103 92
+Output 1:
+Top Student: Karan | Roll: 102 | Marks: 96
+
+*/
+
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+struct Student findTopper(struct Student s[], int n) {
+    int top = 0;
+    for (int i = 1; i < n; i++) {
+        if (s[i].marks > s[top].marks) {
+            top = i;
+        }
+    }
+    return s[top];
+}
+
+int main() {
+    struct Student s[3];
+    int n = 3;
+
+    printf("Enter details of 3 students (Name Roll Marks):\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%s %d %f", s[i].name, &s[i].roll, &s[i].marks);
+    }
+
+    struct Student topper = findTopper(s, n);
+
+    printf("Top Student: %s | Roll: %d | Marks: %.0f\n",
+           topper.name, topper.roll, topper.marks);
+
+    return 0;
+}
